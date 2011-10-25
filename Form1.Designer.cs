894cc3a661b4af094a44e944306da8ebbbe0daa2
Vinicius Guilherme Dal Magro <vinicius.dalmagro@gmail.com>
2011-10-25 10:53:50 -0200

    teste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,14 +52,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 82);
+            this.textBox1.Location = new System.Drawing.Point(74, 108);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(238, 128);
+            this.textBox2.Location = new System.Drawing.Point(248, 84);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
