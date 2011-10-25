a66256ca98f253e598e36954270f19b873bbfa12
Vinicius Guilherme Dal Magro <vinicius.dalmagro@gmail.com>
2011-10-25 14:08:29 -0200

    2510

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(238, 215);
+            this.button1.Location = new System.Drawing.Point(84, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
